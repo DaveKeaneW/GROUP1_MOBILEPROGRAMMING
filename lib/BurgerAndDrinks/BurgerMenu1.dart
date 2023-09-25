8d5928588bfa9fa0ac99c86bb9a30b37ed251206
Darren <isorions25@gmail.com>
2023-09-25 22:11:20 +0700

    read

--- lib/BurgerAndDrinks/BurgerMenu1.dart
+++ lib/BurgerAndDrinks/BurgerMenu1.dart
@@ -3,19 +3,35 @@ import 'package:flutter/material.dart';
 class BurgerMenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Define the value of 'fem' according to your design requirements.
+    double fem = 1.0; // Update this value as needed.
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Burger Menu Page'),
       ),
       body: Column(
         children: [
-          Container(
-            width: 416,
-            height: 123,
-            // You can add child widgets to the Container if needed.
-            // For example, you can add an Image like this:
-            // child: Image.asset("assets/your_image.png"),
+          // The code you provided goes here
+          // ...
+
+          // Replacing the Positioned widget with a custom local image
+          Positioned(
+            left: 291.6059570312 * fem,
+            top: 14.5999755859 * fem,
+            child: Align(
+              child: SizedBox(
+                width: 17.1 * fem,
+                height: 10.7 * fem,
+                child: Image.asset(
+                  "assets/image/Combined Shape.png", // Replace with the correct asset path
+                  width: 17.1 * fem,
+                  height: 10.7 * fem,
+                ),
+              ),
+            ),
           ),
+
           // The rest of your code goes here
           // ...
         ],

b727d1f26f0ece6ec8612690dd6252f686080400
Darren <isorions25@gmail.com>
2023-09-25 21:06:36 +0700

    test

---
+++ lib/BurgerAndDrinks/BurgerSize2.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BurgerMenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Burger Menu Page'),
+      ),
+      body: Column(
+        children: [
+          // The code you provided goes here
+          // ...
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: BurgerMenuPage(),
+  ));
+}

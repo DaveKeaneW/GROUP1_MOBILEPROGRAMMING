543c00c766dc3419f4a9f979ac0c786c74785d8d
kane <kane.53522007@stu.untar.ac.id>
2023-09-26 14:02:03 +0700

    update kane brnktkan

---
+++ lib/cekot.dart
@@ -0,0 +1,644 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class cekot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double fem = 1.0; 
+    return Container(
+      
+width:  double.infinity,
+  height:  928,
+  decoration:  BoxDecoration (
+    color:  Color(0xff201520),
+  ),
+  child:  
+Stack(
+  children:  [
+Positioned(
+  // rectangle169XiP (65:5)
+  left:  5,
+  top:  116,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  375,
+  height:  812,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(33.6231880188),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle172dFd (83:24)
+  left:  10,
+  top:  188,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  343,
+  height:  216,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(12.6086950302),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // orderinvoice8CP (83:9)
+  left:  23,
+  top:  197,
+  child:  
+Container(
+  width:  313,
+  height:  190,
+  child:  
+Column(
+  crossAxisAlignment:  CrossAxisAlignment.center,
+  children:  [
+Container(
+  // autogroupepwjFH1 (CnxfaVbcECnBGdTzofEpWj)
+  padding:  EdgeInsets.fromLTRB(0, 0, 0, 7.51),
+  width:  double.infinity,
+  child:  
+Column(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // autogroupndt5yD1 (CnxdxscwGRGtNayHA5NDT5)
+  margin:  EdgeInsets.fromLTRB(0, 0, 6, 3),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // chickenparmesanspaghettiVh9 (106:349)
+  margin:  EdgeInsets.fromLTRB(0, 0, 53, 0),
+  child:  
+Text(
+  'Chicken Parmesan Spaghetti',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000nRM (106:353)
+  'Rp.70.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogrouprab9iK1 (CnxeMhJF29qvwhyg7ARaB9)
+  margin:  EdgeInsets.fromLTRB(0, 0, 6, 4),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // spaghetticarbonaraeTZ (106:350)
+  margin:  EdgeInsets.fromLTRB(0, 0, 106, 0),
+  child:  
+Text(
+  'Spaghetti Carbonara',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000xDM (106:354)
+  'Rp.55.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogroupjdv1hRq (CnxecSNgFNjkxnHvWUJDV1)
+  margin:  EdgeInsets.fromLTRB(0, 0, 5, 56.85),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // frenchfrieslargedaP (106:351)
+  margin:  EdgeInsets.fromLTRB(0, 0, 100, 0),
+  child:  
+Text(
+  'French Fries ( Large )',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000YhM (106:355)
+  'Rp.40.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogroupufrogoZ (Cnxer6eanddiGVKRuhufro)
+  margin:  EdgeInsets.fromLTRB(0, 0, 8, 3.15),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // deliverychargesRFM (83:12)
+  margin:  EdgeInsets.fromLTRB(0, 0, 129, 0),
+  child:  
+Text(
+  'Delivery Charges',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Container(
+  // rp15000YKy (83:15)
+  margin:  EdgeInsets.fromLTRB(0, 0.15, 0, 0),
+  child:  
+Text(
+  'Rp.15.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+  ],
+),
+),
+Container(
+  // autogroupbaozeP1 (Cnxf1bNm8wmXuZ5d9jbAoZ)
+  margin:  EdgeInsets.fromLTRB(0, 0, 5, 0),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // taxesygB (83:11)
+  margin:  EdgeInsets.fromLTRB(0, 0.49, 205, 0),
+  child:  
+Text(
+  'Taxes',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000VuR (83:14)
+  'Rp.30.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+  ],
+),
+),
+  ],
+),
+),
+Container(
+  // autogroupm2kyeGX (CnxfFLUrxfHAog3xUWm2Ky)
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // grandtotalC39 (83:10)
+  margin:  EdgeInsets.fromLTRB(0, 0, 129, 0),
+  child:  
+Text(
+  'Grand Total',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp210000KdZ (83:13)
+  'Rp.210.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+  ],
+),
+),
+  ],
+),
+),
+),
+Positioned(
+  // cashTzf (83:18)
+  left:  16,
+  top:  595,
+  child:  
+Container(
+  width:  343,
+  height:  45,
+  decoration:  BoxDecoration (
+    color:  Color(0xff543656),
+    borderRadius:  BorderRadius.circular(10),
+    boxShadow:  [
+      BoxShadow(
+        color:  Color(0x26000000),
+        offset:  Offset(2, 2),
+        blurRadius:  7.5,
+      ),
+    ],
+  ),
+  child:  
+Center(
+  child:  
+Center(
+  child:  
+Text(
+  'Shopee Pay ',
+  textAlign:  TextAlign.center,
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+),
+Positioned(
+  // shoopepayHD1 (83:16)
+  left:  16,
+  top:  648,
+  child:  
+Container(
+  width:  343,
+  height:  45,
+  decoration:  BoxDecoration (
+    color:  Color(0xff2e1e2e),
+    borderRadius:  BorderRadius.circular(10),
+    boxShadow:  [
+      BoxShadow(
+        color:  Color(0x26000000),
+        offset:  Offset(2, 2),
+        blurRadius:  7.5,
+      ),
+    ],
+  ),
+  child:  
+Center(
+  child:  
+Center(
+  child:  
+RichText(
+  textAlign:  TextAlign.center,
+  text:  
+TextSpan(
+  style:  GoogleFonts.rosarivo (
+    fontSize:  16,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+  children:  [
+TextSpan(
+  text:  'Cash',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+),
+TextSpan(
+  text:  ' ',
+),
+  ],
+),
+),
+),
+),
+),
+),
+Positioned(
+  // profilepictureyV9 (83:20)
+  left:  286,
+  top:  45,
+  child:  
+Container(
+  padding:  EdgeInsets.fromLTRB(3, 3, 3, 3),
+  width:  50,
+  height:  50,
+  decoration:  BoxDecoration (
+    border:  Border.all(color: Color(0xffdcaa70)),
+    borderRadius:  BorderRadius.circular(25),
+  ),
+  child:  
+Center(
+  // ellipse4783zo (83:22)
+  child:  
+SizedBox(
+  width:  double.infinity,
+  height:  44,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(21.9999980927),
+    image:  DecorationImage (
+      fit:  BoxFit.cover,
+      image:  AssetImage('images/centang.png'),
+    ),
+  ),
+),
+),
+),
+),
+),
+Positioned(
+  // deliveryaddressZTM (83:103)
+  left:  24,
+  top:  429,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  144,
+  height:  23,
+  child:  
+Text(
+  'DELIVERY ADDRESS',
+  style:  GoogleFonts.oleoScript (
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // chooseyourpaymentmethodfWP (83:104)
+  left:  27,
+  top:  569,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  255,
+  height:  23,
+  child:  
+Text(
+  'CHOOSE  YOUR PAYMENT METHOD',
+  style:  GoogleFonts.oleoScript (
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle394AT9 (83:105)
+  left:  11,
+  top:  454,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  343,
+  height:  80,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(20),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // jlgadingnirwanaviiino68jakarta (106:344)
+  left:  27,
+  top:  480,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  291,
+  height:  35,
+  child:  
+Text(
+  'Jl Gading Nirwana VIII No.68, Jakarta Utara, DKI Jakarta   ',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle395ZVH (83:106)
+  left:  16,
+  top:  728,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  337,
+  height:  40,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(20),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // checkoutt1m (83:107)
+  left:  131,
+  top:  736,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  87,
+  height:  23,
+  child:  
+Text(
+  'Check Out',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.15,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // brandlogobB5 (106:392)
+  left:  22,
+  top:  19,
+  child:  
+Container(
+  width:  163,
+  height:  120,
+  child:  
+Stack(
+  children:  [
+Positioned(
+  // dj8Ru (106:393)
+  left:  0,
+  top:  0,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  77,
+  height:  57,
+  child:  
+Text(
+  'déjà',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  40,
+    fontWeight:  FontWeight.w400,
+    height:  1.405,
+    color:  Color(0x7fefe3c8),
+  ),
+),
+),
+),
+),
+Positioned(
+  // brewdNf (106:394)
+  left:  38,
+  top:  46,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  125,
+  height:  74,
+  child:  
+Text(
+  'Brew',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  52,
+    fontWeight:  FontWeight.w400,
+    height:  1.405,
+    color:  Color(0xffefe3c8),
+  ),
+),
+),
+),
+),
+  ],
+),
+),
+),
+Positioned(
+  // yourorder8qD (140:10)
+  left:  22,
+  top:  163,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  98,
+  height:  23,
+  child:  
+Text(
+  'YOUR ORDER',
+  style:  GoogleFonts.oleoScript (
+
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+  ],
+),
+      );
+  }
+}
---
+++ lib/sukses.dart
@@ -0,0 +1,752 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class sukses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double fem = 1.0; 
+    return Container(
+  // kanepunyaJAj (140:12)
+  width:  double.infinity,
+  height:  928,
+  decoration:  BoxDecoration (
+    color:  Color(0xff201520),
+  ),
+  child:  
+Stack(
+  children:  [
+Positioned(
+  // rectangle169mq1 (140:13)
+  left:  5,
+  top:  116,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  375,
+  height:  812,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(33.6231880188),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle172ETh (140:14)
+  left:  10,
+  top:  188,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  343,
+  height:  216,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(12.6086950302),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // orderinvoicei7y (140:15)
+  left:  23,
+  top:  197,
+  child:  
+Container(
+  width:  313,
+  height:  190,
+  child:  
+Column(
+  crossAxisAlignment:  CrossAxisAlignment.center,
+  children:  [
+Container(
+  // autogroupzd6f1cs (Co2xf64xPBGHXLcwnVZD6F)
+  padding:  EdgeInsets.fromLTRB(0, 0, 0, 7.51),
+  width:  double.infinity,
+  child:  
+Column(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // autogroupmbh9XbD (Co2x2STMXGZvTghPb5mbh9)
+  margin:  EdgeInsets.fromLTRB(0, 0, 6, 3),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // chickenparmesanspaghettifSX (140:18)
+  margin:  EdgeInsets.fromLTRB(0, 0, 53, 0),
+  child:  
+Text(
+  'Chicken Parmesan Spaghetti',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000YFR (140:25)
+  'Rp.70.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogroup8j2f4zT (Co2x9MRW5Mdahx9yDF8j2f)
+  margin:  EdgeInsets.fromLTRB(0, 0, 6, 4),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // spaghetticarbonaraCas (140:19)
+  margin:  EdgeInsets.fromLTRB(0, 0, 106, 0),
+  child:  
+Text(
+  'Spaghetti Carbonara',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000hXd (140:26)
+  'Rp.55.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogroupo4ajqtj (Co2xFGRKDwK3qGGdkso4aj)
+  margin:  EdgeInsets.fromLTRB(0, 0, 5, 56.85),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // frenchfrieslargenJB (140:20)
+  margin:  EdgeInsets.fromLTRB(0, 0, 100, 0),
+  child:  
+Text(
+  'French Fries ( Large )',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp3000063y (140:27)
+  'Rp.40.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xffffffff),
+  ),
+),
+  ],
+),
+),
+Container(
+  // autogrouptq8o2TR (Co2xMBR8NWzWxaPJJWTQ8o)
+  margin:  EdgeInsets.fromLTRB(0, 0, 8, 3.15),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // deliverychargesZCT (140:21)
+  margin:  EdgeInsets.fromLTRB(0, 0, 129, 0),
+  child:  
+Text(
+  'Delivery Charges',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Container(
+  // rp15000UaK (140:24)
+  margin:  EdgeInsets.fromLTRB(0, 0.15, 0, 0),
+  child:  
+Text(
+  'Rp.15.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+  ],
+),
+),
+Container(
+  // autogroupqwurCmD (Co2xS6Sc7bHnxwA3mcQwuR)
+  margin:  EdgeInsets.fromLTRB(0, 0, 5, 0),
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // taxeswD1 (140:17)
+  margin:  EdgeInsets.fromLTRB(0, 0.49, 205, 0),
+  child:  
+Text(
+  'Taxes',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp30000Th9 (140:23)
+  'Rp.30.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+  ],
+),
+),
+  ],
+),
+),
+Container(
+  // autogroup3h7u1Tm (Co2xXFxfi3S7kXmH1M3h7u)
+  width:  double.infinity,
+  child:  
+Row(
+  crossAxisAlignment:  CrossAxisAlignment.start,
+  children:  [
+Container(
+  // grandtotalkw9 (140:16)
+  margin:  EdgeInsets.fromLTRB(0, 0, 129, 0),
+  child:  
+Text(
+  'Grand Total',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+),
+Text(
+  // rp210000tGf (140:22)
+  'Rp.210.000',
+  style:  GoogleFonts.inriaSerif (
+    fontSize:  14,
+    fontWeight:  FontWeight.w700,
+    height:  1.1500000272,
+    color:  Color(0xfffbf8f8),
+  ),
+),
+  ],
+),
+),
+  ],
+),
+),
+),
+Positioned(
+  // cashpw1 (140:28)
+  left:  16,
+  top:  595,
+  child:  
+Container(
+  width:  343,
+  height:  45,
+  decoration:  BoxDecoration (
+    color:  Color(0xff543656),
+    borderRadius:  BorderRadius.circular(10),
+    boxShadow:  [
+      BoxShadow(
+        color:  Color(0x26000000),
+        offset:  Offset(2, 2),
+        blurRadius:  7.5,
+      ),
+    ],
+  ),
+  child:  
+Center(
+  child:  
+Center(
+  child:  
+Text(
+  'Shopee Pay ',
+  textAlign:  TextAlign.center,
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+),
+Positioned(
+  // shoopepayTDH (140:30)
+  left:  16,
+  top:  648,
+  child:  
+Container(
+  width:  343,
+  height:  45,
+  decoration:  BoxDecoration (
+    color:  Color(0xff543656),
+    borderRadius:  BorderRadius.circular(10),
+    boxShadow:  [
+      BoxShadow(
+        color:  Color(0x26000000),
+        offset:  Offset(2, 2),
+        blurRadius:  7.5,
+      ),
+    ],
+  ),
+  child:  
+Center(
+  child:  
+Center(
+  child:  
+RichText(
+  textAlign:  TextAlign.center,
+  text:  
+TextSpan(
+  style:  GoogleFonts.rosarivo (
+    fontSize:  16,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+  children:  [
+TextSpan(
+  text:  'Cash',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.2575,
+    color:  Color(0xffffffff),
+  ),
+),
+TextSpan(
+  text:  ' ',
+),
+  ],
+),
+),
+),
+),
+),
+),
+Positioned(
+  // profilepictureVJP (140:32)
+  left:  286,
+  top:  45,
+  child:  
+Container(
+  padding:  EdgeInsets.fromLTRB(3, 3, 3, 3),
+  width:  50,
+  height:  50,
+  decoration:  BoxDecoration (
+    border:  Border.all(color: Color(0xffdcaa70)),
+    borderRadius:  BorderRadius.circular(25),
+  ),
+  child:  
+Center(
+  // ellipse478BBD (140:34)
+  child:  
+SizedBox(
+  width:  double.infinity,
+  height:  44,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(21.9999980927),
+    image:  DecorationImage (
+      fit:  BoxFit.cover,
+      image:  AssetImage ('images/centang.png'
+      ),
+    ),
+  ),
+),
+),
+),
+),
+),
+Positioned(
+  // deliveryaddressgNs (140:35)
+  left:  24,
+  top:  429,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  144,
+  height:  23,
+  child:  
+Text(
+  'DELIVERY ADDRESS',
+  style:  GoogleFonts.oleoScript (
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // chooseyourpaymentmethodyMy (140:36)
+  left:  27,
+  top:  569,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  255,
+  height:  23,
+  child:  
+Text(
+  'CHOOSE  YOUR PAYMENT METHOD',
+  style:  GoogleFonts.oleoScript (
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle394Gby (140:37)
+  left:  11,
+  top:  454,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  343,
+  height:  80,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(20),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // jlgadingnirwanaviiino68jakarta (140:38)
+  left:  27,
+  top:  480,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  291,
+  height:  35,
+  child:  
+Text(
+  'Jl Gading Nirwana VIII No.68, Jakarta Utara, DKI Jakarta   ',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  14,
+    fontWeight:  FontWeight.w400,
+    height:  1.25,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // rectangle395bgB (140:39)
+  left:  16,
+  top:  728,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  337,
+  height:  40,
+  child:  
+Container(
+  decoration:  BoxDecoration (
+    borderRadius:  BorderRadius.circular(20),
+    color:  Color(0xff2e1e2e),
+  ),
+),
+),
+),
+),
+Positioned(
+  // checkouttQP (140:40)
+  left:  131,
+  top:  736,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  87,
+  height:  23,
+  child:  
+Text(
+  'Check Out',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  20,
+    fontWeight:  FontWeight.w500,
+    height:  1.15,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // brandlogoPM9 (140:41)
+  left:  22,
+  top:  19,
+  child:  
+Container(
+  width:  163,
+  height:  120,
+  child:  
+Stack(
+  children:  [
+Positioned(
+  // djuqH (140:42)
+  left:  0,
+  top:  0,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  77,
+  height:  57,
+  child:  
+Text(
+  'déjà',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  40,
+    fontWeight:  FontWeight.w400,
+    height:  1.405,
+    color:  Color(0x7fefe3c8),
+  ),
+),
+),
+),
+),
+Positioned(
+  // brewzbq (140:43)
+  left:  38,
+  top:  46,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  125,
+  height:  74,
+  child:  
+Text(
+  'Brew',
+  style:  GoogleFonts.rosarivo (
+    fontSize:  52,
+    fontWeight:  FontWeight.w400,
+    height:  1.405,
+    color:  Color(0xffefe3c8),
+  ),
+),
+),
+),
+),
+  ],
+),
+),
+),
+Positioned(
+  // yourorder695 (140:44)
+  left:  22,
+  top:  163,
+  child:  
+Align(
+  child:  
+SizedBox(
+  width:  98,
+  height:  23,
+  child:  
+Text(
+  'YOUR ORDER',
+  style:  GoogleFonts.oleoScript (
+    fontSize:  16,
+    fontWeight:  FontWeight.w700,
+    height:  1.385,
+    color:  Color(0xffffffff),
+  ),
+),
+),
+),
+),
+Positioned(
+  // kanepunyaPP5 (65:118)
+  left:  20,
+  top:  194,
+  child:  
+Container(
+  padding:  EdgeInsets.fromLTRB(52.5, 97, 51.5, 42),
+  width:  328,
+  height:  480,
+  decoration:  BoxDecoration (
+    color:  Color(0xf4efe3c8),
+    borderRadius:  BorderRadius.circular(40),
+  ),
+  child:  
+Column(
+  crossAxisAlignment:  CrossAxisAlignment.center,
+  children:  [
+Container(
+  // successiconGBy (83:38)
+  margin:  EdgeInsets.fromLTRB(68.5, 0, 73.5, 6),
+  padding:  EdgeInsets.fromLTRB(28.47, 33.03, 27.33, 31.89),
+  width:  double.infinity,
+  decoration:  BoxDecoration (
+    color:  Color(0xffedb82c),
+    borderRadius:  BorderRadius.circular(56),
+    boxShadow:  [
+      BoxShadow(
+        color:  Color(0xffedb82c),
+        offset:  Offset(0, 4),
+        blurRadius:  2,
+      ),
+    ],
+  ),
+  child:  
+Center(
+  // vector41UYw (83:40)
+  child:  
+SizedBox(
+  width:  26.19,
+  height:  17.08,
+  child:  
+Image.asset(
+  'images/centang.png',
+  width:  26.19,
+  height:  17.08,
+),
+),
+),
+),
+Container(
+  // successoLK (83:41)
+  margin:  EdgeInsets.fromLTRB(0, 0, 2, 56),
+  child:  
+Text(
+  'Success',
+  style:  GoogleFonts.inter (
+    fontSize:  24,
+    fontWeight:  FontWeight.w700,
+    height:  1.5,
+    color:  Color(0xffedb82c),
+  ),
+),
+),
+Container(
+  // paymenthasbeenconfirmedvA3 (83:42)
+  margin:  EdgeInsets.fromLTRB(0, 0, 0, 98),
+  child:  
+Text(
+  'payment has been confirmed',
+  textAlign:  TextAlign.center,
+  style:  GoogleFonts.inter (
+    fontSize:  16,
+    fontWeight:  FontWeight.w500,
+    height:  1.3125,
+    color:  Color(0xff000000),
+  ),
+),
+),
+Text(
+  // yourorderhasbeenreceivedcoZ (83:96)
+  'your order has been received',
+  textAlign:  TextAlign.center,
+  style:  GoogleFonts.inter (
+    fontSize:  16,
+    fontWeight:  FontWeight.w500,
+    height:  1.3125,
+    color:  Color(0xff000000),
+  ),
+),
+Container(
+  // pleasewaitawhilemAf (83:95)
+  margin:  EdgeInsets.fromLTRB(24, 0, 0, 0),
+  child:  
+Text(
+  'please wait a while .....',
+  textAlign:  TextAlign.center,
+  style:  GoogleFonts.inter (
+    fontSize:  16,
+    fontWeight:  FontWeight.w500,
+    height:  1.3125,
+    color:  Color(0xff666c7a),
+  ),
+),
+),
+  ],
+),
+),
+),
+  ],
+),
+); 
+}
+}

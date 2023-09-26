6c3f715c5cd74e5b0c5d9421852bc4467c2d9a7b
DaveKeaneW <daveaja909@gmail.com>
2023-09-26 15:04:58 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tesgit/pagesDave/pagesdave.dart';
 import 'package:tesgit/welcome.dart';
 
 void main() {
@@ -6,14 +7,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : WelcomeScreen(),
+      home : WellcomePage(),
     );
   }
 }
---
+++ lib/pagesDave/pagesdave.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tesgit/pagesDave/createaccount.dart';
+import 'package:tesgit/pagesDave/signin.dart';
+import 'package:tesgit/sharedDave/shared.dart';
+
+
+part 'Welcome_pages.dart';

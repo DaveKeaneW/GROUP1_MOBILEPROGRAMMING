2aec3e64fa4ed27a65498ba7d007997a67ef1684
olippppp <oliviaclarabella29@gmail.com>
2023-09-27 14:02:07 +0700

    oliv5

---
+++ lib/margaritapizza.dart
@@ -0,0 +1,411 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tesgit/menupizza.dart';
+
+class MargaritaPizza extends StatelessWidget {
+  const MargaritaPizza ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 120,
+          backgroundColor: Colors.white,
+          title: Text('Margarita Pizza',
+          style: TextStyle(
+            fontFamily: GoogleFonts.jacquesFrancois().fontFamily,
+            fontSize: 32,
+            color: Colors.black,
+           ),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back,
+            color: Colors.black,
+            size: 32,
+            ),
+            onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => MenuPizza()),
+              );
+            },
+          ),
+        ),
+        body: ListView(children: [
+          MargaritaPizza1(),
+        ],
+        ),
+      ), 
+      );
+  }
+}
+
+
+class MargaritaPizza1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 412,
+          height: 844,
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
+            color: Color(0xFF201520),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                width: 0,
+                strokeAlign: BorderSide.strokeAlignOutside,
+                color: Color(0xFF201520),
+              ),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Color(0x02374151),
+                blurRadius: 48,
+                offset: Offset(6, 7),
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 46,
+                top: 400,
+                child: SizedBox(
+                  width: 304,
+                  height: 80,
+                  child: Text(
+                    'Our Margerita Pizza is best seller of pizza in \nour Deja Brew\'s Store. This pizza usually \ntopped with a variety of things, such as \ntomatoes, cheese, olives, anchovies, and garlic.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 100,
+                top: 520,
+                child: Container(
+                  width: 200,
+                  height: 100,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 3,
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFD9D9D9),
+                            shape: OvalBorder(),
+                            shadows: [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 32,
+                                offset: Offset(0, 16),
+                                spreadRadius: 0,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 38,
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFD9D9D9),
+                            shape: OvalBorder(),
+                            shadows: [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 32,
+                                offset: Offset(0, 16),
+                                spreadRadius: 0,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 73,
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFD9D9D9),
+                            shape: OvalBorder(),
+                            shadows: [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 32,
+                                offset: Offset(0, 16),
+                                spreadRadius: 0,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 32,
+                        top: 0,
+                        child: Container(
+                          width: 185,
+                          height: 25,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: 165,
+                                  height: 25,
+                                  decoration: ShapeDecoration(
+                                    color: Color(0xFFD9D9D9),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    shadows: [
+                                      BoxShadow(
+                                        color: Color(0x3F000000),
+                                        blurRadius: 32,
+                                        offset: Offset(0, 16),
+                                        spreadRadius: 0,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: -5,
+                                top: 4,
+                                child: SizedBox(
+                                  width: 175,
+                                  child: Text(
+                                    'Rp.90.000 (Personal)',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w400,
+                                      height: 0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 32,
+                        top: 35,
+                        child: Container(
+                          width: 175,
+                          height: 25,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: 165,
+                                  height: 25,
+                                  decoration: ShapeDecoration(
+                                    color: Color(0xFFD9D9D9),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: -5,
+                                top: 4,
+                                child: SizedBox(
+                                  width: 175,
+                                  child: Text(
+                                    'Rp.110.000 (Regular)',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w400,
+                                      height: 0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 32,
+                        top: 70,
+                        child: Container(
+                          width: 175,
+                          height: 28,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: 165,
+                                  height: 25,
+                                  decoration: ShapeDecoration(
+                                    color: Color(0xFFD9D9D9),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: -11,
+                                top: 4,
+                                child: SizedBox(
+                                  width: 175,
+                                  child: Text(
+                                    'Rp.130.000 (Large)',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w400,
+                                      height: 0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 50,
+                top: 660,
+                child: Container(
+                  width: 299,
+                  height: 59,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF533556),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    
+                      
+                      
+                    
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 62,
+                top: 668,
+                child: Container(
+                  width: 276,
+                  height: 42,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: 276,
+                          height: 42,
+                          decoration: ShapeDecoration(
+                            color: Colors.white.withOpacity(0),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(width: 1, color: Colors.white),
+                              borderRadius: BorderRadius.circular(45),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 22.38,
+                        top: 12,
+                        child: SizedBox(
+                          width: 231.24,
+                          height: 28,
+                          child: Text(
+                            'ADD TO CART',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.bold,
+                              height: 0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 55, //tambah 10
+                top: 80,
+                child: Container(
+                  width: 300,
+                  height: 283,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/margarita.png'),
+                      fit: BoxFit.fill,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF201520),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 26,
+                top: 61,
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(),
+                ),
+              ),
+             
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}

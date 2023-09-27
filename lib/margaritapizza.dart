eb212a9987cff6a1e2b510526eaba4da2c9ded3f
olippppp <oliviaclarabella29@gmail.com>
2023-09-27 23:00:41 +0700

    oliv8

--- lib/margaritapizza.dart
+++ lib/margaritapizza.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:tesgit/menupizza.dart';
 
 class MargaritaPizza extends StatelessWidget {
-  const MargaritaPizza ({super.key});
+  const MargaritaPizza({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,37 +15,47 @@ class MargaritaPizza extends StatelessWidget {
         appBar: AppBar(
           toolbarHeight: 120,
           backgroundColor: Colors.white,
-          title: Text('Margarita Pizza',
-          style: TextStyle(
-            fontFamily: GoogleFonts.jacquesFrancois().fontFamily,
-            fontSize: 32,
-            color: Colors.black,
-           ),
+          title: Text(
+            'Margarita Pizza',
+            style: TextStyle(
+              fontFamily: GoogleFonts.jacquesFrancois().fontFamily,
+              fontSize: 32,
+              color: Colors.black,
+            ),
           ),
           centerTitle: true,
           leading: IconButton(
-            icon: Icon(Icons.arrow_back,
-            color: Colors.black,
-            size: 32,
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 32,
             ),
-            onPressed: (){
-              Navigator.push(context, 
-              MaterialPageRoute(builder: (context) => MenuPizza()),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MenuPizza()),
               );
             },
           ),
         ),
-        body: ListView(children: [
-          MargaritaPizza1(),
-        ],
+        body: ListView(
+          children: [
+            MargaritaPizza1(),
+          ],
         ),
-      ), 
-      );
+      ),
+    );
   }
 }
 
+class MargaritaPizza1 extends StatefulWidget {
+  @override
+  _MargaritaPizza1State createState() => _MargaritaPizza1State();
+}
 
-class MargaritaPizza1 extends StatelessWidget {
+class _MargaritaPizza1State extends State<MargaritaPizza1> {
+  int _selectedPrice = 1; // 1: Rp.90.000, 2: Rp.110.000, 3: Rp.130.000
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -69,238 +79,43 @@ class MargaritaPizza1 extends StatelessWidget {
                 blurRadius: 48,
                 offset: Offset(6, 7),
                 spreadRadius: 0,
-              )
+              ),
             ],
           ),
           child: Stack(
             children: [
               Positioned(
                 left: 51,
-                top: 370,
+                top: 365,
                 child: SizedBox(
                   width: 304,
                   height: 80,
                   child: Text(
-                    'Our Margerita Pizza is best seller of pizza in \nour Deja Brew\'s Store. This pizza usually \ntopped with a variety of things, such as \ntomatoes, cheese, olives, anchovies, and garlic.',
+                    'Our Margerita Pizza is the best seller of pizza in our Deja Brew\'s Store. This pizza is usually topped with a variety of things, such as tomatoes, cheese, olives, anchovies, and garlic.',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 14,
                       fontFamily: 'Inter',
                       fontWeight: FontWeight.w400,
-                      height: 0,
+                      height: 1.4,
                     ),
                   ),
                 ),
               ),
               Positioned(
-                left: 100,
-                top: 475,
+                left: 50,
+                top: 450,
                 child: Container(
-                  width: 200,
-                  height: 100,
-                  child: Stack(
+                  width: 300,
+                  height: 400,
+                  child: Column(
                     children: [
-                      Positioned(
-                        left: 0,
-                        top: 3,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          decoration: ShapeDecoration(
-                            color: Color(0xFFD9D9D9),
-                            shape: OvalBorder(),
-                            shadows: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 32,
-                                offset: Offset(0, 16),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 0,
-                        top: 38,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          decoration: ShapeDecoration(
-                            color: Color(0xFFD9D9D9),
-                            shape: OvalBorder(),
-                            shadows: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 32,
-                                offset: Offset(0, 16),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 0,
-                        top: 73,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          decoration: ShapeDecoration(
-                            color: Color(0xFFD9D9D9),
-                            shape: OvalBorder(),
-                            shadows: [
-                              BoxShadow(
-                                color: Color(0x3F000000),
-                                blurRadius: 32,
-                                offset: Offset(0, 16),
-                                spreadRadius: 0,
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 32,
-                        top: 0,
-                        child: Container(
-                          width: 185,
-                          height: 25,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                  width: 165,
-                                  height: 25,
-                                  decoration: ShapeDecoration(
-                                    color: Color(0xFFD9D9D9),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                    shadows: [
-                                      BoxShadow(
-                                        color: Color(0x3F000000),
-                                        blurRadius: 32,
-                                        offset: Offset(0, 16),
-                                        spreadRadius: 0,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                left: -5,
-                                top: 4,
-                                child: SizedBox(
-                                  width: 175,
-                                  child: Text(
-                                    'Rp.90.000 (Personal)',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontFamily: 'Inter',
-                                      fontWeight: FontWeight.w400,
-                                      height: 0,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 32,
-                        top: 35,
-                        child: Container(
-                          width: 175,
-                          height: 25,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                  width: 165,
-                                  height: 25,
-                                  decoration: ShapeDecoration(
-                                    color: Color(0xFFD9D9D9),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                left: -5,
-                                top: 4,
-                                child: SizedBox(
-                                  width: 175,
-                                  child: Text(
-                                    'Rp.110.000 (Regular)',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontFamily: 'Inter',
-                                      fontWeight: FontWeight.w400,
-                                      height: 0,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 32,
-                        top: 70,
-                        child: Container(
-                          width: 175,
-                          height: 28,
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                  width: 165,
-                                  height: 25,
-                                  decoration: ShapeDecoration(
-                                    color: Color(0xFFD9D9D9),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                left: -11,
-                                top: 4,
-                                child: SizedBox(
-                                  width: 175,
-                                  child: Text(
-                                    'Rp.130.000 (Large)',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14,
-                                      fontFamily: 'Inter',
-                                      fontWeight: FontWeight.w400,
-                                      height: 0,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+
+                      _buildPriceOption(1, 'Rp.90.000 (Personal)'),
+                      _buildPriceOption(2, 'Rp.110.000 (Regular)'),
+                      _buildPriceOption(3, 'Rp.130.000 (Large)'),
+
                     ],
                   ),
                 ),
@@ -316,10 +131,6 @@ class MargaritaPizza1 extends StatelessWidget {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(45),
                     ),
-                    
-                      
-                      
-                    
                   ),
                 ),
               ),
@@ -370,7 +181,7 @@ class MargaritaPizza1 extends StatelessWidget {
                 ),
               ),
               Positioned(
-                left: 57, //tambah 10
+                left: 57,
                 top: 50,
                 child: Container(
                   width: 300,
@@ -386,7 +197,7 @@ class MargaritaPizza1 extends StatelessWidget {
                         blurRadius: 4,
                         offset: Offset(0, 4),
                         spreadRadius: 0,
-                      )
+                      ),
                     ],
                   ),
                 ),
@@ -401,11 +212,69 @@ class MargaritaPizza1 extends StatelessWidget {
                   decoration: BoxDecoration(),
                 ),
               ),
-             
             ],
           ),
         ),
       ],
     );
   }
+
+Widget _buildPriceOption(int value, String label) {
+  return GestureDetector(
+    onTap: () {
+      setState(() {
+        _selectedPrice = value;
+      });
+    },
+    child: Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: _selectedPrice == value ? Color(0xFF533556) : Colors.white,
+          width: 1,
+        ),
+      ),
+      width: 290,
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _selectedPrice == value ? Color(0xFF533556) : Colors.white,
+              border: Border.all(
+                color: Colors.transparent,
+                width: 2,
+              ),
+            ),
+            child: _selectedPrice == value
+                ? Icon(
+                    Icons.check,
+                    size: 18,
+                    color: Colors.white,
+                  )
+                : null,
+          ),
+          SizedBox(width: 10),
+          Text(
+            label,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontFamily: GoogleFonts.inter().fontFamily,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+
 }

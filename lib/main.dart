397e8dba521839968c339e88f37194d19195898a
Darren <isorions25@gmail.com>
2023-10-03 21:18:16 +0700

    darren

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home : WellcomePage(),
+      home: WelcomeScreen(),
     );
   }
 }
-

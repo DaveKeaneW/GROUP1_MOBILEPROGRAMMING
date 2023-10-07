be86670022405f9527180916d48d93e1c24fd077
DaveKeaneW <daveaja909@gmail.com>
2023-10-08 00:28:30 +0700

    rtes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tesgit/home_screen.dart';
+import 'package:tesgit/cart_provider.dart';
 import 'package:tesgit/pagesDave/pagesdave.dart';
-import 'package:tesgit/tampilanawal.dart';
-import 'package:tesgit/user.dart';
 import 'package:tesgit/welcome.dart';
+import 'tampilanawal.dart';
+import 'package:provider/provider.dart';
+import 'cart_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,14 +15,20 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      home : ProfileAccount(),
+    return ChangeNotifierProvider(
+      create: () => CartProvider(),
+      child: Builder(builder: (BuildContext context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashScreen(),
+        );
+      }),
     );
   }
 }

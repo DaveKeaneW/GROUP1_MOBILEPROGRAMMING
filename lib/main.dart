929825d8dfb7b98f95509b61195c88ed8509421c
Darren <isorions25@gmail.com>
2023-10-07 20:31:30 +0700

    d

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tesgit/home_screen.dart';
 import 'package:tesgit/pagesDave/pagesdave.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,9 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WellcomePage()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: WellcomePage());
   }
 }

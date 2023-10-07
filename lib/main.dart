83dcbe29ca913588c010fa29372a97f41f50c5b9
DaveKeaneW <daveaja909@gmail.com>
2023-10-08 00:28:03 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tesgit/home_screen.dart';
 import 'package:tesgit/pagesDave/pagesdave.dart';
-<<<<<<< HEAD
 import 'package:tesgit/tampilanawal.dart';
+import 'package:tesgit/user.dart';
 import 'package:tesgit/welcome.dart';
-=======
-import 'package:tesgit/welcome.dart';
-import 'tampilanawal.dart';
-import 'package:provider/provider.dart';
-import 'cart_provider.dart';
->>>>>>> 023e1e7ad4ee8c7f2b46257fc38a026643856d37
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,27 +14,14 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home : WellcomePage(),
-=======
-    return ChangeNotifierProvider(
-      create: (_) => CartProvider(),
-      child: Builder(builder: (BuildContext context) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: SplashScreen(),
-        );
-      }),
->>>>>>> 023e1e7ad4ee8c7f2b46257fc38a026643856d37
+      home : ProfileAccount(),
     );
   }
 }

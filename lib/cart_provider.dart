8522ffe490b34776397e6c0705b1bf8020bf66a3
olippppp <oliviaclarabella29@gmail.com>
2023-10-07 19:03:04 +0700

    olip

--- lib/cart_provider.dart
+++ lib/cart_provider.dart
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-import 'package:flutter/foundation.dart';
-
-// Kelas ini akan digunakan untuk mewakili item di keranjang belanja
-class CartItem {
-  final String pizzaName;
-  final double price;
-
-  CartItem({required this.pizzaName, required this.price});
-}
-
-// Kelas ini adalah penyedia data keranjang belanja
-class CartProvider with ChangeNotifier {
-  // Daftar item yang ada di keranjang
-  List<CartItem> _cartItems = [];
-
-  // Mendapatkan daftar item di keranjang
-  List<CartItem> get cartItems => _cartItems;
-
-  // Menambahkan item ke keranjang
-  void addToCart(CartItem item) {
-    _cartItems.add(item);
-    notifyListeners(); // memberi tahu semua yang mendengarkan bahwa data telah berubah
-  }
-
-  // Menghapus item dari keranjang
-  void removeFromCart(CartItem item) {
-    _cartItems.remove(item);
-    notifyListeners(); // memberi tahu semua yang mendengarkan bahwa data telah berubah
-  }
-
-  // Menghitung total harga di keranjang
-  double get totalPrice {
-    double total = 0;
-    for (var item in _cartItems) {
-      total += item.price;
-    }
-    return total;
-  }
-}
-=======
-import '/cartmodel.dart';
+import 'cartmodel.dart';
 import 'db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -113,4 +72,3 @@ class CartProvider with ChangeNotifier{
 
   }
 }
->>>>>>> 5388674f9a65e4ef704368a88984081b07a02dde
